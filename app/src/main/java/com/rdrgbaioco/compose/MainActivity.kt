cc20210f6a781935b411fd21549622d70e003282
Rodrigo Baioco <rdrgbaioco@gmail.com>
2025-04-15 18:01:21 -0300

    Update MainActivity.kt

--- app/src/main/java/com/rdrgbaioco/compose/MainActivity.kt
+++ app/src/main/java/com/rdrgbaioco/compose/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rdrgbaioco.compose.navigation.NavigationGraph
 import com.rdrgbaioco.compose.ui.theme.ComposeTheme
+import org.koin.androidx.compose.KoinAndroidContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +21,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeTheme {
-                NavigationGraph()
+                KoinAndroidContext {
+                    NavigationGraph()
+                }
             }
         }
     }
